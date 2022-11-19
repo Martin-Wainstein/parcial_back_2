13d82bf2dbfd60a7db7549b19ed08137dcf5199d
Martin-Wainstein <mwainstein2003@gmail.com>
2022-11-19 16:45:39 -0300

    subo con el punto 4 empezado

--- movies-api/src/main/java/com/digitalmedia/movies/controller/MoviesController.java
+++ movies-api/src/main/java/com/digitalmedia/movies/controller/MoviesController.java
@@ -9,6 +9,7 @@ import com.digitalmedia.movies.model.dto.UpdateMovieRequest;
 import com.digitalmedia.movies.service.MovieService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class MoviesController {
     private final MovieMapper movieMapper;
 
     @GetMapping
+    @PreAuthorize("hasAnyAuthority('GROUP_user', 'GROUP_client', 'GROUP_admin')")
     public List<MovieDto> getMovies() {
         return movieService.getMovies().stream()
                 .map(movieMapper::toMovieDto)
@@ -42,6 +44,7 @@ public class MoviesController {
     }
 
     @GetMapping("/{imdbId}")
+    @PreAuthorize("hasAnyAuthority('GROUP_user', 'GROUP_client', 'GROUP_admin')")
     public MovieDto getMovie(@PathVariable String imdbId) {
         Movie movie = movieService.validateAndGetMovie(imdbId);
         return movieMapper.toMovieDto(movie);
@@ -50,6 +53,7 @@ public class MoviesController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
+    @PreAuthorize("hasAnyAuthority('GROUP_admin')")
     public MovieDto createMovie(@Valid @RequestBody CreateMovieRequest createMovieRequest) {
         Movie movie = movieMapper.toMovie(createMovieRequest);
         movie = movieService.saveMovie(movie);
@@ -58,6 +62,7 @@ public class MoviesController {
 
 
     @PutMapping("/{imdbId}")
+    @PreAuthorize("hasAnyAuthority('GROUP_admin')")
     public MovieDto updateMovie(@PathVariable String imdbId, @Valid @RequestBody UpdateMovieRequest updateMovieRequest) {
         Movie movie = movieService.validateAndGetMovie(imdbId);
         movieMapper.updateMovieFromDto(updateMovieRequest, movie);
@@ -67,6 +72,7 @@ public class MoviesController {
 
 
     @DeleteMapping("/{imdbId}")
+    @PreAuthorize("hasAnyAuthority('GROUP_admin')")
     public MovieDto deleteMovie(@PathVariable String imdbId) {
         Movie movie = movieService.validateAndGetMovie(imdbId);
         movieService.deleteMovie(movie);
@@ -76,6 +82,7 @@ public class MoviesController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/{imdbId}/comments")
+    @PreAuthorize("hasAnyAuthority('GROUP_user', 'GROUP_client', 'GROUP_admin')")
     public MovieDto addMovieComment(@PathVariable String imdbId, @Valid @RequestBody AddCommentRequest addCommentRequest, Principal principal) {
         Movie movie = movieService.validateAndGetMovie(imdbId);
         Movie.Comment comment = new Movie.Comment(principal.getName(), addCommentRequest.getText(), LocalDateTime.now());
