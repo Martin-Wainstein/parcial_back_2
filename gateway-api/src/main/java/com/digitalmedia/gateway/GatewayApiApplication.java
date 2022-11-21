3f9054f50336383a48569cd9909a94dfd1b48342
Martin-Wainstein <mwainstein2003@gmail.com>
2022-11-20 21:20:20 -0300

    subo todo

--- gateway-api/src/main/java/com/digitalmedia/gateway/GatewayApiApplication.java
+++ gateway-api/src/main/java/com/digitalmedia/gateway/GatewayApiApplication.java
@@ -2,8 +2,11 @@ package com.digitalmedia.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayApiApplication {
 
   public static void main(String[] args) {
